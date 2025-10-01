74c15835d6c08bc7b1f85946adfb83d18a0b3a8c
Ekaterina_Korotkova <i-cho-ter@mail.ru>
2025-10-01 19:13:36 +0500

    added target size in image comression

--- Sources/MVVMCFoundation/Services/ImageCompressionService.swift
+++ Sources/MVVMCFoundation/Services/ImageCompressionService.swift
@@ -10,17 +10,27 @@ import Foundation
 import UIKit
 
 protocol ImageCompressionService {
-    func compressImageData(_ data: Data, maxFileSize: Int) -> Data?
+    func compressImageData(_ data: Data, maxFileSize: Int, targetSize: CGSize?) -> Data?
 }
 
-public final class JPEGCompressionService: ImageCompressionService {
+public final class JPEGCompressionService: @preconcurrency ImageCompressionService {
     public init() {}
     
-    public func compressImageData(_ data: Data, maxFileSize: Int) -> Data? {
-        guard let image = UIImage(data: data) else { return nil }
+    @MainActor
+    public func compressImageData(_ data: Data, maxFileSize: Int, targetSize: CGSize?) -> Data? {
+        guard var image = UIImage(data: data) else { return nil }
+        
+        var size = image.size
+        
+        if let targetSize {
+            if size.width > targetSize.width || size.height > targetSize.height {
+                image = image.resize(targetSize: targetSize)
+            }
+        }
         
         var quality = 1.0
-        var compressedData = data
+        guard var compressedData = image.jpegData(compressionQuality: quality) else { return nil }
+        
         while compressedData.count > maxFileSize {
             quality -= 0.1
             guard let data = image.jpegData(compressionQuality: quality) else { return nil }
