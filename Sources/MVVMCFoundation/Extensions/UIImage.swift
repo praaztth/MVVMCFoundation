f619f1aa14f7bd46db19e4d28e6d8622c70f9094
Ekaterina_Korotkova <i-cho-ter@mail.ru>
2025-10-06 16:32:34 +0500

    method to resize image

---
+++ Sources/MVVMCFoundation/Extensions/UIImage.swift
@@ -0,0 +1,34 @@
+//
+//  UIImage.swift
+//  MVVMCFoundation
+//
+//  Created by катенька on 05.10.2025.
+//
+
+
+import UIKit
+
+public extension UIImage {
+    func resize(targetSize: CGSize) -> UIImage {
+        let size = self.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1.0
+        format.opaque = false
+
+        let renderer = UIGraphicsImageRenderer(size: newSize, format: format)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: newSize))
+        }
+    }
+}
